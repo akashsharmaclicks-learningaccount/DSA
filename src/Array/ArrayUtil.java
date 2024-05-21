ffc2aecc0c06f5c7d80e36bfcb8a595264f4ffee
Akash Sharma <akash.sharma@igniteconnex.com>
2024-05-21 13:23:21 +0530

    Added ArrayUtil class to print elements of an array.

---
+++ src/Array/ArrayUtil.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class ArrayUtil {
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public void arrayDesigner(){
+
+    }
+}
+class Runner_2{
+    public static void main(String[] args) {
+        ArrayUtil obj = new ArrayUtil();
+        int[] dummyArray = {1,2,3,4,5,6,7,8,9};
+        obj.printArray(dummyArray);
+    }
+}
