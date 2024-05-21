1b79066756fd7134f0f7225c95bae2e29d9e141b
Akash Sharma <akash.sharma@igniteconnex.com>
2024-05-21 16:56:29 +0530

    Added a new method to remove odd elements from the array.

--- src/Array/ArrayUtil.java
+++ src/Array/ArrayUtil.java
@@ -32,6 +32,26 @@ public class ArrayUtil {
 
         return resultArr;
     }
+
+    // Method to remove odd numbers from an array
+    public static int[] removeOdd(int[] arr) {
+        int evenElementsCount = 0;
+        for(int i=0; i<arr.length;i++) {
+            if (arr[i] % 2 == 0) {
+                evenElementsCount++;
+            }
+        }
+        int[] resultArr = new int[evenElementsCount];
+        int idx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                resultArr[idx] = arr[i];
+                idx++;
+            }
+
+        }
+        return resultArr;
+    }
 }
 
 class Runner_2 {
@@ -40,10 +60,17 @@ class Runner_2 {
         int[] dummyArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         // Print the original array
+        System.out.println("Dummy Array:");
         obj.printArray(dummyArray);
 
         // Remove even numbers and print the resulting array
-        int[] resultArray = ArrayUtil.removeEven(dummyArray);
-        obj.printArray(resultArray);
+        System.out.println("Dummy Array after removing even elements:");
+        int[] arrayResultWithoutEven = ArrayUtil.removeEven(dummyArray);
+        obj.printArray(arrayResultWithoutEven);
+
+        //Remove odd numbers and print the resulting array
+        System.out.println("Dummy Array after removing odd elements:");
+        int[] arrayResultWithoutOdd = ArrayUtil.removeOdd(dummyArray);
+        obj.printArray(arrayResultWithoutOdd);
     }
 }
