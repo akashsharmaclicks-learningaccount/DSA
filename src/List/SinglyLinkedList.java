7a69f760dd0659214e4e83a9518ac9ac6420b9fa
Akash Sharma <akash.sharma@igniteconnex.com>
2024-07-04 20:29:54 +0530

    Created a Singly Linked List with real data.

--- src/List/SinglyLinkedList.java
+++ src/List/SinglyLinkedList.java
@@ -10,4 +10,16 @@ private static class ListNode{
         this.next = null;
     }
     }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+
+        sll.head.next = second;;
+        second.next = third;
+        third.next = fourth;
+    }
 }
